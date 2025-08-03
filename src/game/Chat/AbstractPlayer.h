fe3ff78a4340420ce87bfcecc248e332af21297d
Melg Eight <public.melg8@gmail.com>
2025-08-03 09:30:30 +0300

    Extract gm flags from player extra flags for future player gm subsystem separation

--- src/game/Chat/AbstractPlayer.h
+++ src/game/Chat/AbstractPlayer.h
@@ -5,16 +5,20 @@
 #include "SharedDefines.h" // For "Team"
 #include "ObjectGuid.h"
 
+enum PlayerExtraGmFlags
+{
+    // gm abilities
+    PLAYER_EXTRA_GM_ON = 0x0001,
+    PLAYER_EXTRA_GM_ACCEPT_TICKETS = 0x0002,
+    PLAYER_EXTRA_ACCEPT_WHISPERS = 0x0004,
+    PLAYER_EXTRA_TAXICHEAT = 0x0008,
+    PLAYER_EXTRA_GM_INVISIBLE = 0x0010,
+    PLAYER_EXTRA_GM_CHAT = 0x0020 // Show GM badge in chat messages
+};
+
 // 2^n values
 enum PlayerExtraFlags
 {
-    // gm abilities
-    PLAYER_EXTRA_GM_ON              = 0x0001,
-    PLAYER_EXTRA_GM_ACCEPT_TICKETS  = 0x0002,
-    PLAYER_EXTRA_ACCEPT_WHISPERS    = 0x0004,
-    PLAYER_EXTRA_TAXICHEAT          = 0x0008,
-    PLAYER_EXTRA_GM_INVISIBLE       = 0x0010,
-    PLAYER_EXTRA_GM_CHAT            = 0x0020,               // Show GM badge in chat messages
     PLAYER_EXTRA_AUCTION_NEUTRAL    = 0x0040,
     PLAYER_EXTRA_AUCTION_ENEMY      = 0x0080,               // overwrite PLAYER_EXTRA_AUCTION_NEUTRAL
 
