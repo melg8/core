7f2dc115657c1098d0c33d1a932ece3f9b3cb6a9
Melg Eight <public.melg8@gmail.com>
2025-08-12 13:17:55 +0300

    Remove USE_PCH_OLD usage from cmake
    By now it would be considered deprecated and not helpful.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -7,6 +7,7 @@ option(USE_SCRIPTS                    "Build scripts"                           
 option(USE_EXTRACTORS                 "Build extractors"                                                                              OFF)
 option(USE_REALMMERGE                 "Build helper tool for merging character databases"                                             OFF)
 option(ENABLE_MAILSENDER              "Enables support for sending emails via sendgrid.com (requires libcurl)"                        OFF)
+option(USE_PCH "Use precompiled headers" ON)
 
 if(ENABLE_ASAN)
   # Prevent usage of tbb malloc which interfieres with sanitizer.
@@ -20,35 +21,6 @@ if(UNIX)
   option(DEBUG_SYMBOLS "Include Debug Symbols" ON)
 endif()
 
-# Precompile headers stuff
-find_package(PCHSupport)
-if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16")
-  option(USE_PCH "Use precompiled headers" ON)
-endif()
-
-# TODO: I have no idea why `USE_PCH_OLD`, is needed if there is `USE_PCH`
-if(PCHSupport_FOUND AND (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.16"))
-  if(MSVC)
-    option(USE_PCH_OLD "Use old precompiled headers implementation" ON)
-  else()
-    option(USE_PCH_OLD "Use old precompiled headers implementation" OFF)
-  endif()
-else()
-  unset(USE_PCH_OLD CACHE)
-endif()
-
-if((${CMAKE_VERSION} VERSION_LESS "3.16") OR USE_PCH_OLD)
-  if(PCHSupport_FOUND)
-    if(WIN32)
-      option(USE_PCH "Use precompiled headers" ON)
-    else()
-      option(USE_PCH "Use precompiled headers" OFF)
-    endif()
-  endif()
-else()
-  option(USE_PCH "Use precompiled headers" ON)
-endif()
-
 if(NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE "Release" CACHE STRING "" FORCE)
 endif()
@@ -60,7 +32,6 @@ message(STATUS
     PREFIX                    Path where the server should be installed to
     CMAKE_INSTALL_PREFIX      Path where the server should be installed to
     USE_PCH                   Use precompiled headers
-    USE_PCH_OLD               Use precompiled headers
     USE_STD_MALLOC            Use standard malloc instead of TBB
     BUILD_FOR_HOST_CPU        Build specifically for the host CPU via `-march=native` (might not run on different machines)
     TBB_DEBUG                 Use TBB debug libraries
